22f8ec0dbe1c1debbefb10794d5170874c9ea05c
Yuza <akhmadyuza@gmail.com>
2023-01-05 20:43:55 +0700

    update temp size

--- src/modules/content.jsx
+++ src/modules/content.jsx
@@ -1,7 +1,13 @@
+import styled from 'styled-components'
+
+const Item = styled.section`
+    height: 100px;
+`
+
 export default function Content(props){
     return props.catData.map((item, index) => (
-        <section key={index}>
+        <Item key={index}>
             <h1>{item.name}</h1>
-        </section>
+        </Item>
     ))
 }
