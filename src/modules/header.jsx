564aeed92080cd6b6143cea7f77e20ddad3b2561
Yuza <akhmadyuza@gmail.com>
2023-01-05 21:17:54 +0700

    search feature logic

--- src/modules/header.jsx
+++ src/modules/header.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const HeaderWrap = styled.section`
@@ -9,11 +10,25 @@ const HeaderWrap = styled.section`
     padding: 0 20px;
 `
 
-export default function Header(){
+export default function Header({stateChanger, catList}){
+    const [inputText, setInputText] = useState('')
+
+    const enterHandler = (event) => {
+        const finder = catList.filter(entry => Object.values(entry).some(val => typeof val === "string" && val.includes(inputText)))
+
+        if (event.key === 'Enter') {
+            stateChanger([...finder])
+        }
+    }
+    
+    const changeHandler = (event) => {
+        setInputText(event.target.value)
+    }
+
     return <HeaderWrap>
         <h1>Cat Showcase</h1>
         <section className='search-bar'>
-            <input type="text" />
+            <input type="text" onChange={changeHandler} value={inputText} onKeyDown={enterHandler}/>
         </section>
     </HeaderWrap>
 }
